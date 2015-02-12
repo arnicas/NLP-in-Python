420988485bd950a23db55d31ab9726d86f6d8aeb
arnicas <lynn@cherny.com>
2015-02-12 09:57:25 -0500

    Node demo fix, Readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # NLP-in-Python
-Intro to some NLP concepts in Python for a class
+
+Intro to some NLP concepts and libraries in Python for a class at CMU,
+Feb 2015.
+
+Lots of libraries are required - see [here](Installation.md) for install info.
+
+Notebook viewer links:
+
+0. [Reading in Files](http://nbviewer.ipython.org/github/arnicas/NLP-in-Python/blob/master/0.%20Reading%20Files.ipynb)
+
+1. [Tokenizing, Stemming, POS](http://nbviewer.ipython.org/github/arnicas/NLP-in-Python/blob/master/1.Tokenizing%2C%20Stemming%2C%20POS.ipynb)
+
+2. [Wordclouds](http://nbviewer.ipython.org/github/arnicas/NLP-in-Python/blob/master/2.%20WordClouds.ipynb)
+
+3. [TF-IDF, Clustering, Pattern](http://nbviewer.ipython.org/github/arnicas/NLP-in-Python/blob/master/3.%20TF-IDF%2C%20Clustering%2C%20Pattern.ipynb)
+
+Doing some TF-IDF NLP in node's package "natural" (but caveats apply):[here](utils/booksNodeTfIdf.js)
+
+4. [Naive Bayes Classification](http://nbviewer.ipython.org/github/arnicas/NLP-in-Python/blob/master/4.%20Naive%20Bayes%20Classification.ipynb) - the infamous 50 Shades Sex Scene Detector
+
+5. [Naive Bayes in Scikit-Learn](http://nbviewer.ipython.org/github/arnicas/NLP-in-Python/blob/master/5.%20Naive%20Bayes%20in%20Scikit-Learn.ipynb)
+
+
--- utils/booksNodeTfIdf.js
+++ utils/booksNodeTfIdf.js
@@ -1,25 +1,33 @@
 
-/*
+/************
 
-This uses code from the natural package, particularly:
+Lynn Cherny, 2/2015:
+
+This uses code from the node natural package, particularly:
 https://github.com/NaturalNode/natural/blob/master/lib/natural/tfidf/tfidf.js
 which implements tf-idf in its own way and removes stopwords, apparently.
+
 https://github.com/NaturalNode/natural/blob/master/lib/natural/util/stopwords.js
 
-*/
+To run this, you need to 
+>npm install natural
+> node booksNodeTfIdf.js <files dir without trailing / ! > <word>
+
+*************/
 
 var natural = require('natural'),
     TfIdf = natural.TfIdf,
     tfidf = new TfIdf();
 
 // Borrowed from Shiffman's node examples for his AtoZ Programming class
-// We can get command line arguments in a node program
+// We can get command line arguments in a node program:
 if (process.argv.length < 4) {
   console.log('Oops, you forgot to pass in a directory path and file to evaluate.');
   process.exit(1);
 }
 
-// The 'fs' (file system) module allows us to read and write files
+// The 'fs' (file system) module allows us to read and write files:
+
 var fs = require('fs');
 // A path for all the files of data
 var path = process.argv[2];
@@ -31,22 +39,6 @@ var files = fs.readdirSync(path);
 // Total word count of document
 var totalwords = 0;
 
-files.forEach(function(d,i) {
-  console.log('Doc #' + i + ' is ' + path + '/' + d);
-  tfidf.addFileSync(path + '/' + d);
-});
-
-setTimeout(function () {
-  // doing it in a timeout because otherwise the files.each above may not have finished
-  files.forEach(function(d,i) {
-    printTopN(i, 6);
-  });
-    console.log('Looking up tfidf for: ' + word);
-  tfidf.tfidfs(word, function(i, measure) {
-      console.log('document #' + i + ' is ' + measure);
-  });
-}, 500);
-
 function printTopN(docNumber, N) {
   var sorted = tfidf.listTerms(docNumber).sort(function(a, b) {
     return b.tfidf - a.tfidf;
@@ -57,4 +49,23 @@ function printTopN(docNumber, N) {
     console.log("  ", a.term, a.tfidf);
   });
 }
+
+// Add each of the files to the model
+
+files.forEach(function(d,i) {
+  console.log('Doc #' + i + ' is ' + path + '/' + d);
+  tfidf.addFileSync(path + '/' + d);
+});
+
+setTimeout(function () {
+  // doing it in a timeout because otherwise the files.each above may not
+  // have finished
+  files.forEach(function(d,i) {
+    printTopN(i, 6);
+  });
+  console.log('Looking up tfidf for: ' + word);
+  tfidf.tfidfs(word, function(i, measure) {
+      console.log('document #' + i + ' with ' + measure);
+  });
+}, 500);
 
